a715fe3f339dc4554305472adaacfb9d17448cce
Paulo Porto <paulo.porto@opah.com.br>
2020-05-04 11:14:12 -0300

    Init todo list with vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Taferas</h1>
+  </div>
+</template>
+
+<script> 
+
+export default {
+ 
+}
+</script>
+
+<style>
+  html {
+    font-family: 'Lato', sans-serif;
+    background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 114));
+    color: white;
+  }
+  #app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  #app h1 {
+    margin-bottom: 5px;
+    font-weight: 300;
+    font-size: 3rem;
+  }
+
+</style>
